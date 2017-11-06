7dffa6bf546c22288866783613257c2b89f0706b
Kevin D. Jones, Ph.D. <kevin.d.jones@outlook.com>
2017-11-05 20:24:52 -0500

    Updated dependencies, assembly versions.

--- openLSEService/Properties/AssemblyInfo.cs
+++ openLSEService/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+// The following GUID is for the ID of the typelib if this project is exposed to COMa
 [assembly: Guid("62bdc1cb-ffab-4985-b9a5-d3c44e623472")]
 
 // Version information for an assembly consists of the following four values:
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.11.0")]
-[assembly: AssemblyVersion("1.0.11.24")]
-[assembly: AssemblyFileVersion("1.0.11.24")]
+[assembly: AssemblyVersion("1.0.11.25")]
+[assembly: AssemblyFileVersion("1.0.11.25")]
